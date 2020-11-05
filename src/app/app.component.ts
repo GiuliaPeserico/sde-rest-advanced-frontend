1271722d8c3d9d58ba969a9bee9edca88e3965b9
Giulia Peserico <giu.peserico@gmail.com>
2020-11-05 08:22:30 +0100

    Base project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'sde-rest-advanced-frontend';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    /*
+      here it is called the http request to the api using the http object injected in the constructor
+      example: this.http.get<>('/api/charts').toPromise().then(() => {});;
+    */
+  }
 }
